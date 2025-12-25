7b97bc68c9c46eabbcea35e4622c43d21dabf90e
Reuel Rai <reuelrai2001@gmail.com>
2025-12-25 23:41:42 +0545

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center h-screen dark:bg-background-100">
         This is the library of unsent letters.
         <button
-          className="cursor-pointer text-white bg-gray-700 dark:text-black dark:bg-white  px-4 py-1 mt-2 rounded hover:bg-gray-800 dark:hover:bg-gray-300 hover:shadow-lg transition-all"
+          className="cursor-pointer text-white bg-gray-700 dark:text-black dark:bg-white  px-4 py-0.5 mt-2 rounded hover:bg-gray-800 dark:hover:bg-gray-300 hover:shadow-lg transition-all"
           onClick={() => router.push("/Dashboard")}>
           Start
         </button>
