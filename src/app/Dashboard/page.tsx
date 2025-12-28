9975786ef7c52f30b60f41129870634c3a022487
Reuel Rai <reuelrai2001@gmail.com>
2025-12-28 21:13:30 +0545

    minor changes

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -1,10 +1,14 @@
 export default function Dashboard() {
     return (
         <>
-        <div className="flex justify-center items-center h-screen">This is dashboard page.
+            <div className="flex justify-center items-center h-screen">This is dashboard page.
+                <br />
+                <h1>Nothing to commit here.</h1>
+                <h2>This is trial.</h2>
+            </div>
             <h1>Nothing to commit here.</h1>
             <h2>This is trial.</h2>
-        </div>
+            <h3>Trial</h3>
         </>
     );
 }
